ee51ba7f6445deb9e2431278bdb1a3e678234099
CLOUDESIGN <kamlesh.bhati@cloudesign.com>
2025-03-04 10:26:35 +0530

    all files download

--- app/config.py
+++ app/config.py
@@ -1,4 +1,6 @@
 from pydantic_settings import BaseSettings
+import json
+from pydantic import field_validator
 
 class AppConfig(BaseSettings):
     DB_HOST: str
@@ -9,9 +11,22 @@ class AppConfig(BaseSettings):
     NSE_BASE_URL: str
     BSE_BASE_URL: str
     DOWNLOAD_DIR: str
+    SOURCE_URL:dict
+
+    @field_validator("SOURCE_URL", mode="before")
+    @classmethod
+    def parse_source_url(cls, value):
+        """Convert JSON string from .env to dictionary"""
+        if isinstance(value, str):
+            try:
+                return json.loads(value)  # Convert JSON string to dict
+            except json.JSONDecodeError as e:
+                raise ValueError(f"Invalid JSON for SOURCE_URL: {e}")
+        return value  # If already a dict, return as is
 
     class Config:
         env_file = ".env"  # Path to your .env file
+        nv_file_encoding = "utf-8"
 
 # Create an instance of the configuration
 app_config = AppConfig()
